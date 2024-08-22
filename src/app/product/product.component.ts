8bef5b0878f6780050d52571787433e4ce5882c7
yasinabdmahmood <abd181103@gmail.com>
2024-08-22 15:58:02 +0300

    DEmonstration of string interpolation in angular

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+   product = {
+    name: 'Shirt',
+    color: 'blue',
+   }
+
+}
